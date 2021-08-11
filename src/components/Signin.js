0c88ad2639e86c3ecc66029d9e752f5f00fd5a29
kmasimini <bradley.kmasimini@gmail.com>
2021-08-11 07:08:06 +0200

    1 file modified, 1 added

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from './AuthContext';
 import { Link, useHistory } from 'react-router-dom';
-import './list.css';
+import './weather.css';
 
 export default function Login() {
   const emailRef = useRef();
