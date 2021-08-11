03c8879a9c03b26a6b6c1b766f6e1d465a81781e
kmasimini <bradley.kmasimini@gmail.com>
2021-08-11 06:54:27 +0200

    1 file added

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from '@firebase/app';
+require('firebase/auth');
+require("firebase/firestore");
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD4X1rWxULVwnd349dagx7xUIEfU1kppAs",
+  authDomain: "todo-list-7c42f.firebaseapp.com",
+  projectId: "todo-list-7c42f",
+  storageBucket: "todo-list-7c42f.appspot.com",
+  messagingSenderId: "546484060528",
+  appId: "1:546484060528:web:0be06fa3d51b2e8b028fb5"
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+export {db};
+export const auth = app.auth();
+export default app;
