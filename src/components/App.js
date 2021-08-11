a829028dd7bbe2926772f77c403687836f9e92d7
kmasimini <bradley.kmasimini@gmail.com>
2021-08-11 06:59:32 +0200

    1 file modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import Signup from './Signup';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from './AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute'
+import TodoList from './TodoList'
+import Calender from "./Calender"
 
 export default function App() {
   return (
-   
+    <div>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: '100vh' }}
+      >
+        <div className="w-100" style={{ maxwidth: '200px' }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+              <PrivateRoute exact path="/" component={TodoList} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/Calender" component={Calender} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+    </div>
   );
 }
