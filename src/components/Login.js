2a0f400e2c51ff86d3a910b1a53e57d4a0bf4b81
kmasimini <bradley.kmasimini@gmail.com>
2021-08-12 08:41:26 +0200

    1 file modified

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from './AuthContext'
 
 
 export default function Login() {
-  return(
-    <div>
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
 
+  async function handleSubmit(e){
+   try{
+        setError("")
+        setLoading(true)
+       await signup(emailRef.current.value, passwordRef.current.value)
+       history.push("/login")
+   }catch {
+       setError('Failed to create an account')
+   }
+
+    setLoading(false)
+   
+ }
+ 
+
+  return (
+    <>
+  
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign up</h2>
+          {error &&<Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Login
+            </Button >
+            <div className="w-100 text-center mt-2">
+        Already have an account? Log In
       </div>
-  )
-
+          </Form>
+        </Card.Body>
+      </Card>
+     
+    </>
+  );
 }
